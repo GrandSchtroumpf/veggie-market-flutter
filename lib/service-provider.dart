2a44f82c46e0c25866a7fefd8caa9467f5ef9a72
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2020-08-09 12:32:34 +0200

    add app performance

--- lib/service-provider.dart
+++ lib/service-provider.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_performance/firebase_performance.dart';
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 import 'seller/service.dart';
@@ -9,6 +10,7 @@ import 'order/bucket.dart';
 /// Provide the list of service for the app
 /// Create the service only on demand
 class ServiceProvider extends InheritedWidget {
+  final Trace trace = FirebasePerformance.instance.newTrace("test_trace");
   final user$ =
       FirebaseAuth.instance.onAuthStateChanged.shareReplay(maxSize: 1);
   final seller = SellerService();
@@ -17,7 +19,9 @@ class ServiceProvider extends InheritedWidget {
   final bucket = Bucket();
 
   ServiceProvider({Key key, @required Widget child})
-      : super(key: key, child: child);
+      : super(key: key, child: child) {
+    trace.start();
+  }
 
   static ServiceProvider of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<ServiceProvider>();
